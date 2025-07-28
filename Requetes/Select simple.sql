ce722ca4c5001490cb584a24738992921bc4156d
Dominique MAURAS <contact@msform.com>
2025-07-28 11:32:46 +0200

    SELECT Simple

---
+++ Requetes/Select simple.sql
@@ -0,0 +1,11 @@
+/*
+	Plusieurs lignes
+	pour expliquer la requète
+	Bonne pratique : Les instructions /mot clé en majuscule 
+*/
+
+SELECT	5+5 AS Calcul_1,
+		3+2 AS [Calcul 2], -- encadrer les noms de colonne avec espace
+		5+2 AS 'Calcul 3', -- pareil
+		'Dominique' AS Nom, -- Bonne pratique : toujour alias
+		'L''eau est fraiche' AS Texte
